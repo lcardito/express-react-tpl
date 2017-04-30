798153cdd97acfcdca9ff6fe61aef7c0738f00e7
Luigi Cardito <lcardito@gmail.com>
2017-04-30 13:07:57 +0100

    chore(): having some fun with bluebird

--- server/src/model/user.js
+++ server/src/model/user.js
@@ -1,9 +1,27 @@
 "use strict";
 
 const bookshelf = require('../config/db').bookshelf;
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcrypt'));
 
 const User = bookshelf.Model.extend({
-    tableName: 'users'
-});
+        tableName: 'users',
+        hidden: ['password']
+    },
+    {
+        login: Promise.method((email, password) => {
+            if (!email || !password) throw new Error('Email and password are both required');
+
+            return new User({email: email.toLowerCase().trim()})
+                .fetch({require: true})
+                .tap((user) => {
+                    return bcrypt.compareAsync(password, user.get('password'))
+                        .then((res) => {
+                            if (!res) throw new Error('Invalid password');
+                        });
+                })
+        })
+    }
+);
 
 module.exports = bookshelf.model('User', User);
